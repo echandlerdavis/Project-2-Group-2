64ef4cf0ab7fa2683f9a8ee3d184f3cc10398db8
Franky Erra <frankyerra43@gmail.com>
2022-07-13 21:33:52 -0700

    franky-recipeRoutes, recipe handlebars, recipe front end js

--- controllers/api/recipeRoutes.js
+++ controllers/api/recipeRoutes.js
@@ -29,4 +29,13 @@ router.get('/:id', async (req, res) => {
     }
   });
 
+  router.post('/', async (req, res) => {
+    try {
+      const recipeData = await Recipe.create(req.body);
+      res.status(200).json(recipeData);
+    } catch (err) {
+      res.status(400).json(err);
+    }
+  });
+
   module.exports = router;
