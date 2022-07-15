fe28b56847f5b4de728cc1c0b3fb39e95ab4c81e
Nolan Flannery <nolanflannery@gmail.com>
2022-07-14 20:22:05 -0700

    fixed conflict

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -3,38 +3,60 @@ const { Recipe, User } = require('../models');
 const withAuth = require('../utils/auth');
 
 router.get('/', withAuth, async (req, res) => {
+  try {
+    // Get all projects and JOIN with user data
+    const recipeData = await Recipe.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ['name'],
+        },
+      ],
+    });
+
+    // Serialize data so the template can read it
+    const recipes = recipeData.map((recipe) => recipe.get({ plain: true }));
+
+    // Pass serialized data and session flag into template
+    res.render('homepage', { 
+      recipes, 
+      logged_in: req.session.logged_in 
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/profile', withAuth, async (req, res) => {
+  try {
+    // Get all recipes and JOIN with user data
+    const recipeData = await Recipe.findByPk(req.session.user_id);
+
+    // Serialize data so the template can read it
+    let recipes;
     try {
-      // Get all projects and JOIN with user data
-      const recipeData = await Recipe.findAll({
-        include: [
-          {
-            model: User,
-            attributes: ['name'],
-          },
-        ],
-      });
-  
-      // Serialize data so the template can read it
-      const recipes = recipeData.map((recipe) => recipe.get({ plain: true }));
-  
-      // Pass serialized data and session flag into template
-      res.render('homepage', { 
-        recipes, 
-        logged_in: req.session.logged_in 
-      });
+      recipes = recipeData.map((recipe) => recipe.get({ plain: true }));
     } catch (err) {
-      res.status(500).json(err);
-    }
-  });
-
-  router.get('/login', (req, res) => {
-    // If the user is already logged in, redirect the request to another route
-    if (req.session.logged_in) {
-      res.redirect('/');
-      return;
+      recipes = [recipeData.get({ plain: true })];
     }
   
-    res.render('login');
-  });
+    // Pass serialized data and session flag into template
+    res.render('profile', { 
+      recipes
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
-  module.exports = router;
+router.get('/login', (req, res) => {
+  // If the user is already logged in, redirect the request to another route
+  if (req.session.logged_in) {
+    res.redirect('/');
+    return;
+  }
+
+  res.render('login');
+});
+
+module.exports = router;
