dd6a0e7221571d5b672f8ff005a54b2dcfaaa335
Franky Erra <frankyerra43@gmail.com>
2022-07-16 10:21:02 -0700

    fixed recipe post route and recipe rendering

--- public/js/newrecipe.js
+++ public/js/newrecipe.js
@@ -1,21 +1,21 @@
 const recipeHandler = async (event) => {
   event.preventDefault();
 
-  const title = document.querySelector('#recipe-title').value.trim();
+  const name = document.querySelector('#recipe-title').value.trim();
   const ingredients = document.querySelector('#recipe-ingredients').value.trim();
   const instructions = document.querySelector('#recipe-instructions').value.trim();
 
-  if (title && ingredients && instructions) {
+  if (name && ingredients && instructions) {
     const response = await fetch(`/api/recipes`, {
       method: 'POST',
-      body: JSON.stringify({ title, ingredients, instructions }),
+      body: JSON.stringify({ name, ingredients, instructions }),
       headers: {
         'Content-Type': 'application/json',
       },
     });
 
     if (response.ok) {
-      document.location.replace('/newrecipe');
+      document.location.replace('/profile');
     } else {
       alert('Failed to create project');
     }
