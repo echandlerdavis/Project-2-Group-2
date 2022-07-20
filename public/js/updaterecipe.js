ed1412f21e6e391f55777778025aff9f07c7b7d6
Chandler Davis <echandlerdavis@gmail.com>
2022-07-19 18:10:38 -0700

    there were local issues i now have fixed

--- public/js/updaterecipe.js
+++ public/js/updaterecipe.js
@@ -1,23 +1,22 @@
 const recipeHandler = async (event) => {
-    event.preventDefault();
+  event.preventDefault();
 
-    const title = document.querySelector('#recipe-title').value.trim();
+  const title = document.querySelector('#recipe-title').value.trim();
   const ingredients = document.querySelector('#recipe-ingredients').value.trim();
   const instructions = document.querySelector('#recipe-instructions').value.trim();
   const recipeId = document.querySelector('#submit').getAttribute('data-id');
 
-    if (recipeId && title && ingredients && instructions) {
-      const response = await fetch(`/api/recipes`, {
+  if (recipeId && title && ingredients && instructions) {
+    const response = await fetch(`/api/recipes/${recipeId}`, {
       method: 'PUT',
-        body: JSON.stringify({ name, ingredients, instructions }),
+      body: JSON.stringify({ 
+        name: title, 
+        ingredients, 
+        instructions }),
       headers: {
         'Content-Type': 'application/json',
       },
     });
-  
-      if (response.ok) {
-        alert('Your recipe has been updated.')
-        document.location.replace('/profile');
 
     if (response.ok) {
       alert('Your recipe has been updated.')
@@ -26,7 +25,7 @@ const recipeHandler = async (event) => {
       alert('Failed to update recipe');
     }
   }
-};}
+};
   
 document
   .querySelector('.box')
